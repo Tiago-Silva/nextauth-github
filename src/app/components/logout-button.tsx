e3717e90e39a564fa95247cb8784d93d22f9687f
Tiago <ti.almeida58@gmail.com>
2024-10-17 13:35:32 -0300

    feat: creating logout-button.tsx

---
+++ src/app/components/logout-button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { signOut } from 'next-auth/react';
+
+const LogoutButton = () => {
+    const handleClick = () => {
+        signOut();
+    }
+
+    return (
+        <button
+            className="bg-black text-white px-10 py-2 font-light text-xl rounded-lg"
+        >
+            Logout
+        </button>
+    );
+};
+
+export default LogoutButton;
