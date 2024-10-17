787050672253801cc2809028267ad8efed17823d
Tiago <ti.almeida58@gmail.com>
2024-10-17 09:19:26 -0300

    feat: creating login-button.tsx

---
+++ src/app/components/login-button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {FaGithub} from "react-icons/fa";
+
+const LoginButton = () => {
+    return (
+        <button
+            className="mt-24 bg-black text-white px-6 py-3 flex items-center gap-3 rounded-lg mx-auto"
+        >
+            <FaGithub />
+            Login com Github
+        </button>
+    );
+};
+
+export default LoginButton;
