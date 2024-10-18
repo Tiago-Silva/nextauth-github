f859563bd5e520619a59255d85f5928c7df4e392
Tiago <ti.almeida58@gmail.com>
2024-10-18 09:06:12 -0300

    feat: adding redirect

--- src/app/components/login-button.tsx
+++ src/app/components/login-button.tsx
@@ -1,10 +1,16 @@
 'use client';
 import React from 'react';
 import {FaGithub} from "react-icons/fa";
+import { signIn } from "next-auth/react";
 
 const LoginButton = () => {
+    const handleClick = () => {
+        signIn('github');
+    }
+
     return (
         <button
+            onClick={handleClick}
             className="mt-24 bg-black text-white px-6 py-3 flex items-center gap-3 rounded-lg mx-auto"
         >
             <FaGithub />
