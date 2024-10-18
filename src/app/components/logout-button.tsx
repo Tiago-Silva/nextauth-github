fb3e470cf26909ecc8446150a4fba9066ada7d52
Tiago <ti.almeida58@gmail.com>
2024-10-18 09:06:14 -0300

    feat: adding redirect

--- src/app/components/logout-button.tsx
+++ src/app/components/logout-button.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import { signOut } from 'next-auth/react';
 
@@ -8,6 +9,7 @@ const LogoutButton = () => {
 
     return (
         <button
+            onClick={handleClick}
             className="bg-black text-white px-10 py-2 font-light text-xl rounded-lg"
         >
             Logout
